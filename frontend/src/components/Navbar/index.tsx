ec94968e0b69e6560dc85cc9875a7fbfeecc426a
Thayna Santana <88935936+ThaynaSantana@users.noreply.github.com>
2022-03-13 19:33:14 -0300

    agr foi tudo certo

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                     <a href="https://github.com/ThaynaSantana">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/ThaynaSantana</p>
+                            <p className="dsmovie-contact-link"><b>/ThaynaSantana</b></p>
                         </div>
                     </a>
                 </div>
